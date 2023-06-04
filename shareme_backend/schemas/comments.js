20ed8952485ccc50d74795f29b8854e6109a7d33
bharathzt <bharath.hy@zeksta.com>
2023-06-04 19:29:33 +0530

    first commit

---
+++ shareme_backend/schemas/comments.js
@@ -0,0 +1,17 @@
+export default {
+    name: "comments",
+    title: "Commnts",
+    type: "document",
+    fields: [
+        {
+            name: "postedBy",
+            title: "PostedBy",
+            type: "postedBy",
+        },
+        {
+            name: "comments",
+            title: "Comments",
+            type: "string",
+        }
+    ]
+}
